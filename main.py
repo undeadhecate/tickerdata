4674ffe26911385b8fe9e4f117c767fd3d915521
undeadhecate <86398409+undeadhecate@users.noreply.github.com>
2024-11-02 10:51:07 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from typing import Union
+from selenium import webdriver
+import lxml.etree
 from lxml import html
 from fastapi import FastAPI
 import requests
@@ -8,13 +10,18 @@ app = FastAPI()
 
 @app.get("/quote/{ticker}/{exchange}")
 def read_root(ticker: str,exchange: str):
-    response =requests.get("https://www.google.com/finance/quote/"+ticker+":"+exchange)
-    tree = html.fromstring(response.content)
-    selector = CSSSelector('.AHmHk > span:nth-child(1) > div:nth-child(1) > div:nth-child(1)')
-    elements = selector(tree)
-    for value in elements:
-        pass
-    return {"ticker": ticker,"exchange":exchange,'value':value.text,'asd':'asdf'}
+    url =("https://www.google.com/finance/quote/"+ticker+":"+exchange)
+    xpath = '/html/body/c-wiz[2]/div/div[4]/div/main/div[2]/div[1]/div[1]/c-wiz/div/div[1]/div/div[1]/div/div[1]/div/span/div/div'
+
+    browser = webdriver.Firefox()
+    browser.get(url)
+    html_source = browser.page_source
+
+    tree = html.fromstring(html_source)
+    text = tree.xpath(xpath)
+    for i in text:
+        print(i.text)
+    return {"ticker": ticker,"exchange":exchange,'value':text[0].text,'asd':'asdf'}
 
 
 @app.get("/items/{item_id}")
